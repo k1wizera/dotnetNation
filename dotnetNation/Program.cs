bd881d782bffcdc6e27329d43a494b0a406fd66d
Otávio Faria <oaugustofaria@gmail.com>
2023-03-01 00:24:59 -0300

    Identity & Admin Seed

--- dotnetNation/Program.cs
+++ dotnetNation/Program.cs
@@ -1,11 +1,48 @@
 using dotnetNation;
+using dotnetNation.Data;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
+
 
 var startup = new Startup(builder.Configuration);
 startup.ConfigureServices(builder.Services);
 
 var app = builder.Build();
+try
+{
+    var scope = app.Services.CreateScope();
+    var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+    var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+
+    context.Database.EnsureCreated();
+
+    var adminRole = new IdentityRole("Admin");
+    if (!context.Roles.Any())
+    {
+        //Create a Role
+        roleManager.CreateAsync(adminRole).GetAwaiter().GetResult();
+    }
+
+    if (!context.Users.Any(u => u.UserName == "admin"))
+    {
+        //Create an admin
+        var adminUser = new IdentityUser()
+        {
+            UserName = "admin",
+            Email = "dotnetnation@gmail.com"
+        };
+        userManager.CreateAsync(adminUser, "password").GetAwaiter().GetResult();
+        //Add Role to User
+        userManager.AddToRoleAsync(adminUser, adminRole.Name).GetAwaiter().GetResult();
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
 
 startup.Configure(app, app.Environment);
 app.Run();
--- dotnetNation/Startup.cs
+++ dotnetNation/Startup.cs
@@ -1,6 +1,10 @@
 ﻿using dotnetNation.Data;
 using dotnetNation.Data.Repository;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity.UI;
+
 
 namespace dotnetNation
 {
@@ -16,15 +20,26 @@ namespace dotnetNation
         {
             services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
-            services.AddTransient<IRepository, Repository>();
+            services.AddDefaultIdentity<IdentityUser>(options =>
+            {
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireDigit = false;
+                options.Password.RequiredLength = 6; 
+            }).AddRoles<IdentityRole>().AddEntityFrameworkStores<AppDbContext>();
 
+            services.AddTransient<IRepository, Repository>();
+            
             services.AddMvc(options => options.EnableEndpointRouting = false);
         }
 
-        public void Configure(WebApplication app, IWebHostEnvironment environment)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment environment)
         {
             if (environment.IsDevelopment())
                 app.UseDeveloperExceptionPage();
+
+            app.UseAuthentication();
 
             app.UseMvcWithDefaultRoute();
         }
